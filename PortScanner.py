2f9716422a7cd8ff44ce2f20322f8c270985b0dc
Ayan Dey <ayan98026@gmail.com>
2023-04-10 20:36:48 +0530

    Adding comments for better understandding

--- PortScanner.py
+++ PortScanner.py
@@ -1,25 +1,28 @@
-import socket
-from IPy import IP
+import socket # module for low-level networkting functionality 
+              # allowing to communicate over the internet using various network protocols, such as TCP and UDP
+from IPy import IP # IP address manipulation    
 
 def scan(target):
-    converted_ip = check_ip(target)
+    converted_ip = check_ip(target) # takes a target IP address or domain name as input, converts it to an IP address (if necessary)
     print("\n Scanning target/s: "+str(target))
     for port in range(1,100):
-        scan_port(converted_ip, port)
+        scan_port(converted_ip, port) # scans for open_ports
 
 def check_ip(ip):
     try:
-        IP(ip)
+        IP(ip) # checks if the input is a valid IP address or domain name, and returns the IP address if the input is a domain name
         return(ip)
     except ValueError:
         return socket.gethostbyname(ip)
 
+# A banner is a message or string of text that is sent by a server or application when a connection is established with a client. 
+# The banner typically contains information about the server or application, such as its name, version number, and other details.
 def get_banner(sck):
     return sck.recv(1024)
 
-def scan_port(ipaddress, port):
+def scan_port(ipaddress, port): # creates a socket
     try:
-        sock = socket.socket()
+        sock = socket.socket() # function is used to create a new socket object that can be used to establish a connection with another program or device over a network.
         sock.settimeout(0.5)
         sock.connect((ipaddress,port))
         try:
